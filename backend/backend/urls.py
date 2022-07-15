f55d6d531e6a8cf473867e7f20db8d1d8ab59f78
Ivan <afivan@bk.ru>
2022-07-15 17:15:04 +0300

    added ReDoc

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,16 +18,6 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf.urls import url
-
-
-
-urlpatterns = [
-    path('', include('auto.urls')),
-    path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-]
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -39,12 +29,15 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns += [
-   url(r'^swagger(?P<format>\.json|\.yaml)$', 
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
-       name='schema-swagger-ui'),
-   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+
+urlpatterns = [
+    path('', include('auto.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
        name='schema-redoc'),
-] 
+]
+
+
 
